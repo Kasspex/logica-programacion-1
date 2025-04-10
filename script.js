01cfe64abb23db8188e32af66603f7344bb591f1
Kasspex <feliphomezparda@gmail.com>
2025-04-09 20:05:14 -0500

    correcion de errores

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const prompt = require("prompt-sync")();
 
-let numero = prompt("Ingrese 3 numeros: ")
-let numeros = numero.split(",")
-let ordenados = []
-ordenados.push(Math.max(...numeros));
+let input = prompt("Ingrese 3 números separados por coma: "); // ejemplo: 5,8,3
 
-for(let i = 0; i <= 3; i++){
-if(numeros[i] != Math.max(...numeros) && numeros[i] != Math.min(...numeros) && numeros[i] && numeros[i] != undefined) {
-    ordenados.push(Number(numeros[i]));
-    
-}else if(Math.max(...numeros) == numeros[i] || Math.min(...numeros) == numeros[i]){
-    ordenados.push(Number(numeros[i]));
-    break;
-}
-}
+// Convertimos los strings a números
+let numeros = input.split(",").map(Number);
 
-ordenados.push(Math.min(...numeros));
+// Orden ascendente
+let ordenados = numeros.slice().sort((a, b) => a - b);
 
 console.log(ordenados);
+
+// Orden descendente
 console.log(ordenados.slice().reverse());
 
 
